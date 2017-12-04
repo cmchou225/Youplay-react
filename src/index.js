de8375abd6885ada7710eee4577f4fd2a9c04d3c
Charles <cming.chou@gmail.com>
2017-12-04 13:45:25 -0800

    removed completed files for initial project committ + privatized API Key

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  console.log(API_KEY);
+  return(
+  <div>
+    <SearchBar />
+  </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('react-root')); 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack');
+const myEnv = require('dotenv').config();
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -10,17 +13,22 @@ module.exports = {
   module: {
     loaders: [{
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin ({
+      API_KEY: JSON.stringify(myEnv.parsed.API_KEY)
+    })
+  ]
 };
